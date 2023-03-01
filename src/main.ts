f821f4da9999ed02197ca37b9242c2b3be726594
Agus Basari <agusbasari29@gmail.com>
2023-03-02 02:27:51 +0700

    progress

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,38 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import firebase from 'firebase/app'
-import 'firebase/database'
+// import firebase from 'firebase/app'
+// import 'firebase/database'
 
 import App from './App.vue'
 import router from './router'
 
-firebase.initializeApp({
-    apiKey: "AIzaSyDIP8yeFsgHm30xuKlxZ4kOSEl3Y-CY3Ic",
-    authDomain: "jabrixofthings-c1c16.firebaseapp.com",
-    databaseURL: "https://jabrixofthings-c1c16-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "jabrixofthings-c1c16",
-    storageBucket: "jabrixofthings-c1c16.appspot.com",
-    messagingSenderId: "1024004792418",
-    appId: "1:1024004792418:web:abd6765f8d0b7efee9b13b",
-    measurementId: "G-W9YC90TVDY"
+import { initializeApp, applicationDefault, cert } from 'firebase-admin/app'
+import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore'
+// firebase.initializeApp({
+//     apiKey: "AIzaSyDIP8yeFsgHm30xuKlxZ4kOSEl3Y-CY3Ic",
+//     authDomain: "jabrixofthings-c1c16.firebaseapp.com",
+//     databaseURL: "https://jabrixofthings-c1c16-default-rtdb.asia-southeast1.firebasedatabase.app",
+//     projectId: "jabrixofthings-c1c16",
+//     storageBucket: "jabrixofthings-c1c16.appspot.com",
+//     messagingSenderId: "1024004792418",
+//     appId: "1:1024004792418:web:abd6765f8d0b7efee9b13b",
+//     measurementId: "G-W9YC90TVDY"
+// })
+
+initializeApp({
+    credential: applicationDefault()
 })
 
-import './assets/main.css'
 
-export const db = firebase.database()
+import './assets/main.css'
+// export const db = firebase.database()
+
 
 const app = createApp(App)
+export const db = getFirestore()
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
